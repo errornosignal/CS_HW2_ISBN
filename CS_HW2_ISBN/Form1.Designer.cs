f0944fe9019603248afba5ab048c9852d75e11ef
errornosignal <errornosignal@users.noreply.github.com>
2018-02-10 01:14:44 -0600

    02 10 2018 01:14am

--- CS_HW2_ISBN/Form1.Designer.cs
+++ CS_HW2_ISBN/Form1.Designer.cs
@@ -28,12 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Isbn10MaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.Isbn13MaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.SelectionComboBox = new System.Windows.Forms.ComboBox();
+            this.SelectFormatLabel = new System.Windows.Forms.Label();
+            this.ValidateIsbnButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ValidLabel = new System.Windows.Forms.Label();
+            this.InvalidLabel = new System.Windows.Forms.Label();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.ClearFormButton = new System.Windows.Forms.Button();
+            this.SubmittedIsbnTextBox = new System.Windows.Forms.TextBox();
+            this.InputIsbnLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Isbn10MaskedTextBox
+            // 
+            this.Isbn10MaskedTextBox.BeepOnError = true;
+            this.Isbn10MaskedTextBox.Location = new System.Drawing.Point(88, 120);
+            this.Isbn10MaskedTextBox.Mask = "0-00-000000-0";
+            this.Isbn10MaskedTextBox.Name = "Isbn10MaskedTextBox";
+            this.Isbn10MaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.Isbn10MaskedTextBox.TabIndex = 0;
+            this.Isbn10MaskedTextBox.TabStop = false;
+            this.Isbn10MaskedTextBox.Visible = false;
+            this.Isbn10MaskedTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Isbn10MaskedTextBox_KeyDown);
+            this.Isbn10MaskedTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Isbn10MaskedTextBox_MouseDown);
+            // 
+            // Isbn13MaskedTextBox
+            // 
+            this.Isbn13MaskedTextBox.BeepOnError = true;
+            this.Isbn13MaskedTextBox.Location = new System.Drawing.Point(88, 120);
+            this.Isbn13MaskedTextBox.Mask = "000-0-00-000000-0";
+            this.Isbn13MaskedTextBox.Name = "Isbn13MaskedTextBox";
+            this.Isbn13MaskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.Isbn13MaskedTextBox.TabIndex = 0;
+            this.Isbn13MaskedTextBox.Visible = false;
+            this.Isbn13MaskedTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Isbn13MaskedTextBox_KeyDown);
+            this.Isbn13MaskedTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Isbn13MaskedTextBox_MouseDown);
+            // 
+            // SelectionComboBox
+            // 
+            this.SelectionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SelectionComboBox.FormattingEnabled = true;
+            this.SelectionComboBox.Items.AddRange(new object[] {
+            "ISBN-13",
+            "ISBN-10"});
+            this.SelectionComboBox.Location = new System.Drawing.Point(90, 25);
+            this.SelectionComboBox.Name = "SelectionComboBox";
+            this.SelectionComboBox.Size = new System.Drawing.Size(100, 21);
+            this.SelectionComboBox.TabIndex = 1;
+            this.SelectionComboBox.SelectedIndexChanged += new System.EventHandler(this.SelectionComboBox_SelectedIndexChanged);
+            // 
+            // SelectFormatLabel
+            // 
+            this.SelectFormatLabel.AutoSize = true;
+            this.SelectFormatLabel.Location = new System.Drawing.Point(103, 9);
+            this.SelectFormatLabel.Name = "SelectFormatLabel";
+            this.SelectFormatLabel.Size = new System.Drawing.Size(70, 13);
+            this.SelectFormatLabel.TabIndex = 5;
+            this.SelectFormatLabel.Text = "ISBN Format:";
+            // 
+            // ValidateIsbnButton
+            // 
+            this.ValidateIsbnButton.Location = new System.Drawing.Point(88, 80);
+            this.ValidateIsbnButton.Name = "ValidateIsbnButton";
+            this.ValidateIsbnButton.Size = new System.Drawing.Size(100, 23);
+            this.ValidateIsbnButton.TabIndex = 2;
+            this.ValidateIsbnButton.Text = "Validate ISBN";
+            this.ValidateIsbnButton.UseVisualStyleBackColor = true;
+            this.ValidateIsbnButton.Click += new System.EventHandler(this.ValidateIsbnButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ExitButton.Location = new System.Drawing.Point(3, 80);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Visible = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ValidLabel
+            // 
+            this.ValidLabel.AutoSize = true;
+            this.ValidLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.ValidLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ValidLabel.ForeColor = System.Drawing.Color.Green;
+            this.ValidLabel.Location = new System.Drawing.Point(26, 120);
+            this.ValidLabel.Name = "ValidLabel";
+            this.ValidLabel.Size = new System.Drawing.Size(43, 15);
+            this.ValidLabel.TabIndex = 6;
+            this.ValidLabel.Text = "Valid!";
+            this.ValidLabel.Visible = false;
+            // 
+            // InvalidLabel
+            // 
+            this.InvalidLabel.AutoSize = true;
+            this.InvalidLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.InvalidLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InvalidLabel.ForeColor = System.Drawing.Color.Red;
+            this.InvalidLabel.Location = new System.Drawing.Point(16, 120);
+            this.InvalidLabel.Name = "InvalidLabel";
+            this.InvalidLabel.Size = new System.Drawing.Size(53, 15);
+            this.InvalidLabel.TabIndex = 7;
+            this.InvalidLabel.Text = "Invalid!";
+            this.InvalidLabel.Visible = false;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Enabled = false;
+            this.OutputTextBox.Location = new System.Drawing.Point(88, 190);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.OutputTextBox.TabIndex = 8;
+            this.OutputTextBox.TabStop = false;
+            // 
+            // ClearFormButton
+            // 
+            this.ClearFormButton.Location = new System.Drawing.Point(3, 25);
+            this.ClearFormButton.Name = "ClearFormButton";
+            this.ClearFormButton.Size = new System.Drawing.Size(75, 21);
+            this.ClearFormButton.TabIndex = 3;
+            this.ClearFormButton.Text = "Clear Form";
+            this.ClearFormButton.UseVisualStyleBackColor = true;
+            this.ClearFormButton.Click += new System.EventHandler(this.ClearFormButton_Click);
+            // 
+            // SubmittedIsbnTextBox
+            // 
+            this.SubmittedIsbnTextBox.Enabled = false;
+            this.SubmittedIsbnTextBox.Location = new System.Drawing.Point(88, 155);
+            this.SubmittedIsbnTextBox.Name = "SubmittedIsbnTextBox";
+            this.SubmittedIsbnTextBox.ReadOnly = true;
+            this.SubmittedIsbnTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SubmittedIsbnTextBox.TabIndex = 9;
+            // 
+            // InputIsbnLabel
+            // 
+            this.InputIsbnLabel.AutoSize = true;
+            this.InputIsbnLabel.Location = new System.Drawing.Point(-1, 158);
+            this.InputIsbnLabel.Name = "InputIsbnLabel";
+            this.InputIsbnLabel.Size = new System.Drawing.Size(85, 13);
+            this.InputIsbnLabel.TabIndex = 10;
+            this.InputIsbnLabel.Text = "Submitted ISBN:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Corrected ISBN:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.ValidateIsbnButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.CancelButton = this.ExitButton;
+            this.ClientSize = new System.Drawing.Size(200, 222);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputIsbnLabel);
+            this.Controls.Add(this.SubmittedIsbnTextBox);
+            this.Controls.Add(this.ClearFormButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InvalidLabel);
+            this.Controls.Add(this.ValidLabel);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ValidateIsbnButton);
+            this.Controls.Add(this.SelectFormatLabel);
+            this.Controls.Add(this.SelectionComboBox);
+            this.Controls.Add(this.Isbn13MaskedTextBox);
+            this.Controls.Add(this.Isbn10MaskedTextBox);
+            this.Name = "Form1";
+            this.Text = "ISBN Validator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox Isbn10MaskedTextBox;
+        private System.Windows.Forms.MaskedTextBox Isbn13MaskedTextBox;
+        private System.Windows.Forms.ComboBox SelectionComboBox;
+        private System.Windows.Forms.Label SelectFormatLabel;
+        private System.Windows.Forms.Button ValidateIsbnButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label ValidLabel;
+        private System.Windows.Forms.Label InvalidLabel;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Button ClearFormButton;
+        private System.Windows.Forms.TextBox SubmittedIsbnTextBox;
+        private System.Windows.Forms.Label InputIsbnLabel;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CS_HW2_ISBN/Form1.cs
+++ CS_HW2_ISBN/Form1.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,272 @@ namespace CS_HW2_ISBN
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Isbn10 label constant.
+        /// </summary>
+        private const string Isbn10LabelString = "ISBN-10";
+
+        /// <summary>
+        /// Isbn13 label constant.
+        /// </summary>
+        private const string Isbn13LabelString = "ISBN-13";
+
+        /// <summary>
+        /// Isbn conversion prefix. (Must remain "9","7","8")
+        /// </summary>
+        private const string UpConvertString = "978";
+
+        /// <summary>
+        /// Sets initial form state on Form1_Load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SelectionComboBox.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Sets correct form state for SelectionComboBox_SelectedIndexChanged event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.SelectionComboBox.SelectedItem.ToString() == Isbn10LabelString)
+            {
+                this.Isbn10MaskedTextBox.Visible = true;
+                this.Isbn10MaskedTextBox.Focus();
+                this.Isbn13MaskedTextBox.Visible = false;
+                this.Isbn13MaskedTextBox.Clear();
+            }
+            else if (this.SelectionComboBox.SelectedItem.ToString() == Isbn13LabelString)
+            {
+                this.Isbn13MaskedTextBox.Visible = true;
+                this.Isbn13MaskedTextBox.Focus();
+                this.Isbn10MaskedTextBox.Visible = false;
+                this.Isbn10MaskedTextBox.Clear();
+            }
+            else {/*doNothing()*/} //No other possible cases
+        }
+
+
+//FIX ME---------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Runs methods for data entry, validation, calculating ISBN check-digits, and setting form state.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ValidateIsbnButton_Click(object sender, EventArgs e)
+        {
+            this.ValidLabel.Visible = false;
+            this.InvalidLabel.Visible = false;
+            var IsbnIsValid = false;
+            var IsbnToValidate = "";
+
+            if (this.SelectionComboBox.SelectedItem.ToString() == Isbn10LabelString)
+            {
+                IsbnToValidate = StripStringToDigits(this.Isbn10MaskedTextBox.Text);
+                this.SubmittedIsbnTextBox.Text = this.Isbn10MaskedTextBox.Text;
+                this.Isbn10MaskedTextBox.Clear();
+                this.Isbn10MaskedTextBox.Focus();
+            }
+            else if (this.SelectionComboBox.SelectedItem.ToString() == Isbn13LabelString)
+            {
+                IsbnToValidate = StripStringToDigits(this.Isbn13MaskedTextBox.Text);
+                this.SubmittedIsbnTextBox.Text = this.Isbn13MaskedTextBox.Text;
+                this.Isbn13MaskedTextBox.Clear();
+                this.Isbn13MaskedTextBox.Focus();
+            }
+            else {/*doNothing()*/} //No other possible cases
+
+            IsbnIsValid = ValidateIsbn(IsbnToValidate, out var FinalIsbn);
+
+            if (IsbnIsValid)
+            {
+                try
+                {
+                    FinalIsbn = FinalIsbn.Insert(3, "-").Insert(5, "-").Insert(8, "-").Insert(15, "-");
+                    this.OutputTextBox.Text = FinalIsbn;
+                    this.ValidLabel.Visible = true;
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine(Ex.Message);
+                    this.InvalidLabel.Visible = true;
+                    DisplayInvalidDataMessage(SelectionComboBox.SelectedItem.ToString());
+                    this.OutputTextBox.Clear();
+                }
+            }
+            else if (!IsbnIsValid)
+            {
+                SubmittedIsbnTextBox.Clear();
+                this.InvalidLabel.Visible = true;
+                MessageBox.Show("No data has been entered!", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            else {/*doNothing()*/} //No other possible cases
+        }
+        
+        /// <summary>
+        /// Removes dashes from string with regular expression.
+        /// </summary>
+        /// <param name="Str"></param>
+        /// <returns></returns>
+        private string StripStringToDigits(string Str)
+        {
+            var StrippedStr = Regex.Replace(Str, @"\D", "");
+            return StrippedStr;
+        }
+
+        //SEEMS TO BE WORKING AS EXPECTED
+        //TODO: Still need to add ISBN validation calculation formulas
+        private bool ValidateIsbn(string IsbnToValidate, out string FinalIsbn)
+        {
+            char[] IsbnArray = IsbnToValidate.ToCharArray();
+
+            if (this.SelectionComboBox.SelectedItem.ToString() == Isbn10LabelString && IsbnArray.Length > 0)
+            {
+                FinalIsbn = UpConvertString + string.Join("", IsbnArray);
+                return true;
+            }
+            else if (this.SelectionComboBox.SelectedItem.ToString() == Isbn13LabelString && IsbnArray.Length > 0)
+            {
+                FinalIsbn = string.Join("", IsbnArray);
+                return true;
+            }
+            else
+            {
+                FinalIsbn = "";
+                return false;
+            }
+        }
+
+        //END OF FIX ME--------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Displays error MessageBox on Invalid data entered.
+        /// </summary>
+        /// <param name="IsbnFormat"></param>
+        private void DisplayInvalidDataMessage(string IsbnFormat)
+        {
+            MessageBox.Show("Input was incomplete!\n" +
+                            "1. Verify that the proper ISBN format is selected.\n" +
+                            "2. Enter the complete ISBN.\n" +
+                            "3. Click 'Validate ISBN'.", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            SubmittedIsbnTextBox.Clear();
+
+            if (IsbnFormat == Isbn10LabelString)
+            {
+                this.Isbn10MaskedTextBox.Clear();
+                this.Isbn10MaskedTextBox.Focus();
+            }
+            else if(IsbnFormat == Isbn13LabelString)
+            {
+                this.Isbn13MaskedTextBox.Clear();
+                this.Isbn13MaskedTextBox.Focus();
+            }
+            else {/*doNothing()*/} //No other possible cases
+        }
+
+        /// <summary>
+        /// Resets the form state on the ClearFormButton_Click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearFormButton_Click(object sender, EventArgs e)
+        {
+            this.ValidLabel.Visible = false;
+            this.InvalidLabel.Visible = false;
+            this.Isbn10MaskedTextBox.Clear();
+            this.Isbn13MaskedTextBox.Clear();
+            this.SubmittedIsbnTextBox.Clear();
+            this.OutputTextBox.Clear();
+
+            if (this.SelectionComboBox.SelectedItem.ToString() == Isbn10LabelString)
+            {
+                this.Isbn10MaskedTextBox.Focus();
+            }
+            else if (this.SelectionComboBox.SelectedItem.ToString() == Isbn13LabelString)
+            {
+                this.Isbn13MaskedTextBox.Focus();
+            }
+            else {/*doNothing()*/} //No other possible cases
+        }
+
+        /// <summary>
+        /// Clears and refocuses Isbn10MaskedTextBox on Isbn10MaskedTextBox_MouseDown event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Isbn10MaskedTextBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            Isbn10MaskedTextBox.Clear();
+            Isbn10MaskedTextBox.Focus();
+        }
+
+        /// <summary>
+        /// Clears and refocuses Isbn13MaskedTextBox on Isbn13MaskedTextBox_MouseDown event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Isbn13MaskedTextBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            Isbn13MaskedTextBox.Clear();
+            Isbn13MaskedTextBox.Focus();
+        }
+
+        /// <summary>
+        /// Disable 'Space', 'Left Arrow', 'Right Arrow', 'Up Arrow', and 'Down Arrow' keys 
+        /// w/BeepOnError on Isbn10MaskedTextBox_KeyDown event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Isbn10MaskedTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right
+                || e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                SystemSounds.Beep.Play();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+            else {/*doNothing()*/} //No other cases needed
+        }
+
+        /// <summary>
+        /// Disable 'Space', 'Left Arrow', 'Right Arrow', 'Up Arrow', and 'Down Arrow' keys 
+        /// w/BeepOnError on Isbn13MaskedTextBox_KeyDown event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Isbn13MaskedTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right 
+                || e.KeyCode == Keys.Up || e.KeyCode == Keys.Down)
+            {
+                SystemSounds.Beep.Play();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+            else {/*doNothing()*/} //No other cases needed
+        }
+
+        /// <summary>
+        /// Closes the form on ExitButton_Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
